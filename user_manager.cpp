4457f8ab4200ff35db46d793aad45cf37a47bb9a
trodevel <trodevel@gmail.com>
2019-03-10 01:50:02 +0100

    adjusted to new utils

--- user_manager.cpp
+++ user_manager.cpp
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 9338 $ $Date:: 2018-06-08 #$ $Author: serge $
+// $Revision: 10615 $ $Date:: 2019-03-10 #$ $Author: serge $
 
 #include "user_manager.h"               // self
 
@@ -27,7 +27,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "utils/mutex_helper.h"         // MUTEX_SCOPE_LOCK
 #include "utils/dummy_logger.h"         // dummy_log
-#include "utils/assert.h"               // ASSERT
+#include "utils/utils_assert.h"               // ASSERT
 
 #include "serializer.h"                 // serializer::load
 
